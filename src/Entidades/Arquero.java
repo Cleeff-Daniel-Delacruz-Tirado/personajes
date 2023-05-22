8092ba4fbe35d198f7ba8e8d013bebcd9514866f
Cleeff-Daniel-Delacruz-Tirado <129539428+Cleeff-Daniel-Delacruz-Tirado@users.noreply.github.com>
2023-05-22 16:17:38 -0500

    Add files via upload

---
+++ src/Entidades/Arquero.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @Cleeff Daniel De la cruz Tirado
+ */
+
+    public class Arquero extends Personaje {
+
+    private int agilidad;
+    private int precision;
+
+   
+    @Override
+    public void atacar(Personaje atacado) {
+        int nivelActualDeVida = atacado.getVida();
+        if (this.precision>5) {
+            atacado.setVida(nivelActualDeVida - 50);
+        }else
+            atacado.setVida(nivelActualDeVida - 6);
+        atacado.defender(this);  
+    }
+  
+    public int getAgilidad() {
+        return agilidad;
+    }
+
+    public void setAgilidad(int agilidad) {
+        this.agilidad = agilidad;
+    }
+
+    public int getPrecision() {
+        return precision;
+    }
+
+    public void setPrecision(int precision) {
+        this.precision = precision;
+    }
+
+    
+  }
+
